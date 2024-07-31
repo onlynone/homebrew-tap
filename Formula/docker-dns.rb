14f45065eb8d46aeb31b2cdc395aec0763c1df00
Steven Willis <onlynone@gmail.com>
2024-07-31 15:33:30 -0400

    Upgrade docker-dns to v1.0.1

--- Formula/docker-dns.rb
+++ Formula/docker-dns.rb
@@ -4,8 +4,8 @@ class DockerDns < Formula
 
   desc "Make docker containers addressable via DNS"
   homepage "https://gitlab.com/onlynone/docker-mac-dns"
-  url "https://github.com/onlynone/docker-mac-dns/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "f4a2624c931606d33c050f0a8d3ab695ae0e14526aec2afb2e6d9a59b75d5bd8"
+  url "https://github.com/onlynone/docker-mac-dns/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "c6510e6f12b26ffcce5d3796bc1105c8fb31d8fa8b8053a58dcd50eee6162ec5"
 
   depends_on "python@3"
   depends_on "chipmk/tap/docker-mac-net-connect"
