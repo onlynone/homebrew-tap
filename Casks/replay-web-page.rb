7783b7d18f3f756b98ed5dd8193f5d12471d95da
Steven Willis <steven@philo.com>
2025-01-21 12:15:29 -0500

    these were sitting in the worktree, but not committed

---
+++ Casks/replay-web-page.rb
@@ -0,0 +1,11 @@
+cask "replay-web-page" do
+  version "1.5.7"
+  sha256 "d847fbfae2c268443309774e99cd407ce6a0ef470edde7dc710db1c5cb65e71f"
+
+  url "https://github.com/webrecorder/replayweb.page/releases/download/v#{version}/ReplayWeb.page-#{version}.dmg"
+  name "replay-web-page"
+  desc "Web archive replay system as a single web site"
+  homepage "https://replayweb.page/"
+
+  app "ReplayWeb.page.app"
+end
