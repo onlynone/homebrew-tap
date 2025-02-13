c8a009ed37a36562f14d4b4afdee7ff403eec945
Steven Willis <steven@philo.com>
2025-02-13 10:38:17 -0500

    suitest-drive: set sha256 to :no_check

--- Casks/suitest-drive.rb
+++ Casks/suitest-drive.rb
@@ -1,6 +1,6 @@
 cask "suitest-drive" do
   version "2.9.4"
-  sha256 "87ae5a31181b568fe53b8e388b8eab506c2175051b979ae55767e19be3d5a9c8"
+  sha256 :no_check
 
   url "https://file.suite.st/suitestdrive/latest/mac"
   name "suitest-drive"
